65d15493680fa0f6073948276295f7b48f9ac91d
zenix-s <sergiofs19@gmail.com>
2024-01-11 12:38:41 +0100

    Add ft_tolower function and fix ft_toupper function

--- libft/ft_tolower.c
+++ libft/ft_tolower.c
@@ -1,32 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_toupper.c                                       :+:      :+:    :+:   */
+/*   ft_tolower.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: serferna <serferna@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/11 12:18:42 by serferna          #+#    #+#             */
-/*   Updated: 2024/01/11 12:30:34 by serferna         ###   ########.fr       */
+/*   Created: 2024/01/11 12:30:40 by serferna          #+#    #+#             */
+/*   Updated: 2024/01/11 12:31:12 by serferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /**
- * Convierte una letra minúscula a mayúscula.
+ * Convierte una letra mayúscula a minúscula.
  * @param c Caracter a convertir.
  * @return Caracter convertido.
 */
-int ft_toupper(int c) 
+int ft_tolower(int c)
 {
-  if (c >= 'a' && c <= 'z') 
+  if (c >= 'A' && c <= 'Z')
   {
-    c -= 32;
+    c += 32;
   }
   return c;
 }
 
-// int main(void)
-// {
-//   char c = 'a';
-//   printf("%c\n", ft_toupper(c));
-//   return 0;
-// }
+int main(void)
+{
+  char c = 'A';
+  printf("%c\n", ft_tolower(c));
+  return 0;
+}
