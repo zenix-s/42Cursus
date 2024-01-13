e73af1111dfb73dfe25c064febd0e546a57e9b6a
zenix-s <sergiofs19@gmail.com>
2024-01-13 22:01:39 +0100

    Add ft_strjoin function to libft

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,39 @@
+#include "libft.h"
+
+char *ft_strjoin(char const *s1, char const *s2)
+{
+	char *str;
+	size_t len1;
+	size_t len2;
+	size_t i;
+
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	str = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (i < len1)
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	i = 0;
+	while (i < len2)
+	{
+		str[i + len1] = s2[i];
+		i++;
+	}
+	str[len1 + len2] = '\0';
+	return (str);
+}
+
+int main(void)
+{
+	char *s1 = "Hola";
+	char *s2 = "Mundo";
+	char *s3 = ft_strjoin(s1, s2);
+	printf("%s\n", s3);
+	free(s3);
+	return (0);
+}
