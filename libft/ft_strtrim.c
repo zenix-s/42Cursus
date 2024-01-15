9c5b4ff74b316b6f99c34ab2af361e25c30f7063
zenix-s <sergiofs19@gmail.com>
2024-01-15 16:42:09 +0100

    Refactor ft_strtrim function

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,18 +6,18 @@
 /*   By: serferna <serferna@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 11:13:43 by serferna          #+#    #+#             */
-/*   Updated: 2024/01/15 16:35:45 by serferna         ###   ########.fr       */
+/*   Updated: 2024/01/15 16:38:19 by serferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strtrim(const char *str, const char *set)
+char	*ft_strtrim(const char *str, const char *set)
 {
-	char *new_str;
-	int len;
-	int start;
-	int end;
+	char	*new_str;
+	int		len;
+	int		start;
+	int		end;
 
 	len = ft_strlen(str);
 	start = 0;
@@ -30,14 +30,3 @@ char *ft_strtrim(const char *str, const char *set)
 	new_str = ft_substr(str, start, len);
 	return (new_str);
 }
-
-// int main(void)
-// {
-// 	char *st = "* * * *Hola mundo* * * *";
-// 	char *set = "* ";
-// 	char *new_st = ft_strtrim(st, set);
-// 	printf("%s\n", new_st);
-// 	// printf("strtrim: %s\n", strtrim(st, set));
-// 	free(new_st);
-// 	return 0;
-// }
