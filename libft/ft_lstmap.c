ace4cc2ec4e2caedae5ba435577ee1677d48009e
zenix-s <sergiofs19@gmail.com>
2024-01-23 10:48:10 +0100

    Refactor ft_lstmap function to improve performance

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: serferna <serferna@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 09:50:27 by serferna          #+#    #+#             */
-/*   Updated: 2024/01/23 10:25:15 by serferna         ###   ########.fr       */
+/*   Updated: 2024/01/23 10:47:52 by serferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,13 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	new_lst = NULL;
 	while (lst)
 	{
-		new_node = ft_lstnew((*f)(lst->content));
+		new_node = ft_lstnew(lst->content);
 		if (!new_node)
 		{
 			ft_lstclear(&new_lst, del);
 			return (NULL);
 		}
+		new_node -> content = f(lst->content);
 		ft_lstadd_back(&new_lst, new_node);
 		lst = lst->next;
 	}
