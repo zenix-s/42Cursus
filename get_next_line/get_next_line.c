cfa132054a14c64a63038ea644fd7de322721904
zenix-s <sergiofs19@gmail.com>
2024-02-01 10:31:08 +0100

    up

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: serferna <serferna@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 13:08:09 by serferna          #+#    #+#             */
-/*   Updated: 2024/02/01 11:47:22 by serferna         ###   ########.fr       */
+/*   Updated: 2024/02/01 10:25:40 by serferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,44 +22,12 @@ void ft_free_gnl(char *str, char *buffer, char *aux)
 		free(aux);
 }
 
-char *get_next_line(int fd)
+char *gnl_str_lj(char *str, char *buffer)
 {
-	
-	static char *str;
-	char *buffer;
 	char *aux;
 	int i;
 	int j;
 
-	i = 0;
-	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
-	if (!buffer)
-		return (NULL);
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, buffer, 0) < 0)
-		return (NULL);
-	if (!str)
-		str = ft_strdup("");
-	
-	while ((i = read(fd, buffer, BUFFER_SIZE)) > 0)
-	{
-		buffer[i] = '\0';
-		aux = ft_strjoin(str, buffer);
-		free(str);
-		str = aux;
-		if (ft_strchr(str, '\n'))
-			break ;
-	}
-	free(buffer);
-	if (i < 0)
-	{
-		ft_free_gnl(str, buffer, aux);
-		return (NULL);
-	}
-	if (!i && !ft_strlen(str))
-	{
-		ft_free_gnl(str, buffer, aux);
-		return (NULL);
-	}
 	i = 0;
 	while (str[i] != '\n' && str[i] != '\0')
 		i++;
@@ -73,6 +41,62 @@ char *get_next_line(int fd)
 	free(str);
 	str = buffer;
 	free(buffer);
+	return (aux);
+}
+
+char *get_next_line(int fd)
+{
+	
+	static char *str[1024];
+	char *buffer;
+	char *aux;
+	int i;
+	int j;
+
+	i = 0;
+	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	if (!buffer)
+		return (NULL);
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, buffer, 0) < 0)
+		return (NULL);
+	if (!str[fd])
+		str[fd] = ft_strdup("");
+	
+	if (ft_strchr(str[fd], '\n'))
+		return (gnl_str_lj(str[fd], buffer));
+
+	while ((i = read(fd, buffer, BUFFER_SIZE)) > 0)
+	{
+		buffer[i] = '\0';
+		aux = ft_strjoin(str[fd], buffer);
+		free(str[fd]);
+		str[fd] = aux;
+		if (ft_strchr(str[fd], '\n'))
+			break ;
+	}
+	free(buffer);
+	if (i < 0)
+	{
+		ft_free_gnl(str[fd], buffer, aux);
+		return (NULL);
+	}
+	if (!i && !ft_strlen(str[fd]))
+	{
+		ft_free_gnl(str[fd], buffer, aux);
+		return (NULL);
+	}
+	i = 0;
+	while (str[fd][i] != '\n' && str[fd][i] != '\0')
+		i++;
+	aux = ft_substr(str[fd], 0, i);
+	if (str[fd][i] == '\n')
+		i++;
+	j = 0;
+	while (str[fd][i + j] != '\0')
+		j++;
+	buffer = ft_substr(str[fd], i, j);
+	// free(str);
+	str[fd] = buffer;
 	return (aux);
 }
 
