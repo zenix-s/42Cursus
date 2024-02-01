9c6068384bc895c9402dde7fe85da8fbf0860cc9
zenix-s <sergiofs19@gmail.com>
2024-02-01 11:18:59 +0100

    up

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: serferna <serferna@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 13:08:09 by serferna          #+#    #+#             */
-/*   Updated: 2024/02/01 10:25:40 by serferna         ###   ########.fr       */
+/*   Updated: 2024/02/01 10:45:37 by serferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,34 +46,56 @@ char *gnl_str_lj(char *str, char *buffer)
 
 char *get_next_line(int fd)
 {
-	
+
 	static char *str[1024];
 	char *buffer;
 	char *aux;
 	int i;
 	int j;
 
-	i = 0;
-	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
-	if (!buffer)
-		return (NULL);
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, buffer, 0) < 0)
+	i = 1;
+
+	//
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	if (!str[fd])
 		str[fd] = ft_strdup("");
-	
-	if (ft_strchr(str[fd], '\n'))
-		return (gnl_str_lj(str[fd], buffer));
-
-	while ((i = read(fd, buffer, BUFFER_SIZE)) > 0)
+	if (!str[fd])
+		return (NULL);
+	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	if (!buffer)
 	{
+		free(str[fd]);
+		return (NULL);
+	}
+
+	// while ((i = read(fd, buffer, BUFFER_SIZE)) > 0)
+	while (i && !ft_strchr(str[fd], '\n'))
+	{
+		i = read(fd, buffer, BUFFER_SIZE);
+		if (i < 0)
+		{
+			free(buffer);
+			free(str[fd]);
+			return (NULL);
+		}
 		buffer[i] = '\0';
 		aux = ft_strjoin(str[fd], buffer);
 		free(str[fd]);
+		if (!aux)
+		{
+			free(buffer);
+			return (NULL);
+		}
+		free (buffer);
+
+
 		str[fd] = aux;
 		if (ft_strchr(str[fd], '\n'))
-			break ;
+			break;
 	}
+	
 	free(buffer);
 	if (i < 0)
 	{
@@ -99,4 +121,3 @@ char *get_next_line(int fd)
 	str[fd] = buffer;
 	return (aux);
 }
-
