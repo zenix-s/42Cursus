88436c7018122c328228c8de1acebcca6ce26f11
zenix-s <sergiofs19@gmail.com>
2024-03-26 11:10:58 +0100

    Refactor file I/O and add additional file descriptor***

--- get_next_line/gnl_tester.c
+++ get_next_line/gnl_tester.c
@@ -2,22 +2,57 @@
 
 
 
+// int main(void)
+// {
+// 	int fd;
+// 	char *line;
+
+// 	fd = open("quijote.txt", O_RDONLY);
+
+// 	line = get_next_line(fd);
+// 	while (line)
+// 	{
+// 		printf("%s", line);
+// 		free(line);
+// 		line = get_next_line(fd);
+// 	}
+// 	free(line);
+// 	close(fd);
+
+// 	// system("leaks a.out");
+
+// 	return (0);
+// }
+
 int main(void)
 {
-	int fd;
+	int fd, fd2;
 	char *line;
 
 	fd = open("quijote.txt", O_RDONLY);
+	fd2 = open("text.txt", O_RDONLY);
 
-	line = get_next_line(fd);
+
+	line = get_next_line(0);
+	// while (line)
+	// {
+	// 	printf("%s", line);
+	// 	free(line);
+	// 	line = get_next_line(0);
+	// }
+	free(line);
+	close(fd);
+
+	line = get_next_line(fd2);
 	while (line)
 	{
 		printf("%s", line);
 		free(line);
-		line = get_next_line(fd);
+		line = get_next_line(fd2);
 	}
 	free(line);
-	close(fd);
+	close(fd2);
+
 
 	// system("leaks a.out");
 
